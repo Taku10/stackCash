7f814cc75c67e3b8f0733686d174e8095d8f1993
Takunda Madziwa <tvmadziwa5@gmail.com>
2025-02-27 21:32:16 -0600

    enhance transaction history component; add account selection dropdown and improve styles

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -4,32 +4,46 @@ import '../styles/history.css';
 import Transaction from './Transaction';
 
 const History = () => {
- 
+
     const [currentPage, setCurrentPage] = useState(1);
     const transactionsPerPage = 10;
-// Calculate the indices for the current page
-const indexOfLastTransaction = currentPage * transactionsPerPage;
-const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
-const currentTransactions = transactionData.slice(indexOfFirstTransaction, indexOfLastTransaction);
+    // Calculate the indices for the current page
+    const indexOfLastTransaction = currentPage * transactionsPerPage;
+    const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
+    const currentTransactions = transactionData.slice(indexOfFirstTransaction, indexOfLastTransaction);
 
     // Calculate total pages
     const totalPages = Math.ceil(transactionData.length / transactionsPerPage);
 
-     // Handle page change
-     const handlePageChange = (pageNumber) => {
+    // Handle page change
+    const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
 
     return (
-        
+
         <div className="history-container">
-            <h1>Transaction History</h1>
-             <div  className='transaction-list'>
-             {currentTransactions.map((transaction) => (
+            <div className='history-header'>
+                <h1>Transaction History</h1>
+                <div className="select-account">
+                    <select name="accounts" id="accounts">
+                        <option >Select Account</option>
+                        <option value="chase">Chase</option>
+                        <option value="wellsFargo">Wells Fargo</option>
+                        <option value="usBank">Us Bank</option>
+                    </select>
+                </div>
+            </div>
+
+            <div>
+                
+            </div>
+            <div className='transaction-list'>
+                {currentTransactions.map((transaction) => (
                     <Transaction key={transaction.id} transaction={transaction} />
                 ))}
-             </div>
-             <div className="pagination">
+            </div>
+            <div className="pagination">
                 {[...Array(totalPages)].map((_, index) => (
                     <button
                         key={index}
@@ -41,8 +55,8 @@ const currentTransactions = transactionData.slice(indexOfFirstTransaction, index
                 ))}
             </div>
         </div>
-        
-        
+
+
     );
 };
 
