445fbd6e0db1b7768cc10268a951c692d1407f00
Takunda Madziwa <tvmadziwa5@gmail.com>
2025-02-28 13:53:34 -0600

    add Transfer component; update styles for transaction list, pagination, and pie chart

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -41,151 +41,154 @@ const Overview = () => {
     }
 
     return (
-    
-            <div className='home-wrapper'>
-                <div className='left-middle-wrapper'>
-                    <div className='welcome-section'>
-                        <h1>
-                            Welcome <span className='name'>Takunda</span>
-                        </h1>
-                        <p>
-                            Inspired by Stack Overflow, but stacking money
-                        </p>
-                    </div>
-                    <div className='balance-section'>
-                    <Doughnut
-                                data={{
-                                    labels: ["Chase", "Wells Fargo", "US Bank"],
-                                    datasets: [
-                                        {
-                                            label: "Banks",
-                                            data: [344, 500, 200],
-                                            backgroundColor: ["#58d68d", "#82e0aa", "#28b463"],
-                                            offset	: 10,
-                                        },
-                                    ],
-                                }}
-                                options={{
-                                    responsive: true,
-                                    maintainAspectRatio: false,
-                                    cutout: '50%',
-                                    plugins: {
-                                        legend: {
-                                            display: false
-                                        }
+
+        <div className='home-wrapper'>
+            <div className='left-middle-wrapper'>
+                <div className='welcome-section'>
+                    <h1>
+                        Welcome <span className='name'>Takunda</span>
+                    </h1>
+                    <p>
+                        Inspired by Stack Overflow, but stacking money
+                    </p>
+                </div>
+                <div className='balance-section'>
+                    {/* <div className='pie-header'> */}
+                        <Doughnut
+                            data={{
+                                labels: ["Chase", "Wells Fargo", "US Bank"],
+                                datasets: [
+                                    {
+                                        label: "Banks",
+                                        data: [344, 500, 200],
+                                        backgroundColor: ["#58d68d", "#82e0aa", "#28b463"],
+                                        offset: 10,
+                                    },
+                                ],
+                            }}
+                            options={{
+                                responsive: true,
+                                maintainAspectRatio: false,
+                                cutout: '50%',
+                                plugins: {
+                                    legend: {
+                                        display: false
                                     }
-                                }}
-                                className='pie-chart'
-                            />
-                        <div className="bank-stuff">
-                            <p className="num-bank">
-                                3 Bank Accounts
+                                }
+                            }}
+                            className='pie-chart-banks'
+                        />
+                    {/* </div> */}
+
+                    <div className="bank-stuff">
+                        <p className="num-bank">
+                            3 Bank Accounts
+                        </p>
+                        <div className="total-balance">
+                            <p>
+                                Total Current Balance
                             </p>
-                            <div className="total-balance">
-                                <p>
-                                    Total Current Balance
-                                </p>
-                                <h3>
-                                   $<CountUp  end={875.45} decimals={2} duration={2.4}/> 
-                                </h3>
-                            </div>
+                            <h3>
+                                $<CountUp end={875.45} decimals={2} duration={2.4} />
+                            </h3>
+                        </div>
 
-                        </div>
-                        <div className="add-bank">
-                            <GoPlus className="plus" />
-                            <p>Add Bank</p>
-                        </div>
                     </div>
-                    <div className="transaction-section">
-                        <div className="transaction-header header">
-                            <h4>Transactions</h4>
-                            <button>View all</button>
-                        </div>
-                        <div className="transaction-wrapper">
-                            <div className="record">
-                                <div className="record-left">
-                                    <img src={roger} alt="" />
-                                    <p>Roger <span></span> </p>
-                                </div>
-                                <p className="record-amount-received">+$43.30</p>
-                                <p className="record-date">02/08/2025</p>
-                            </div>
-                            <div className="record">
-                                <div className="record-left">
-                                    <img src={walmart} alt="" />
-                                    <p>Walmart</p>
-                                </div>
-                                <p className="record-amount-spent">-$53.87</p>
-                                <p className="record-date">02/04/2025</p>
-                            </div>
-                            <div className="record">
-                                <div className="record-left">
-                                    <img src={netflix} alt="" />
-                                    <p>Netflix</p>
-                                </div>
-                                <p className="record-amount-spent">-$13.99</p>
-                                <p className="record-date">02/02/2025</p>
-                            </div>
-                            <div className="record">
-                                <div className="record-left">
-                                    <img src={chess} alt="" />
-                                    <p>Chess.com</p>
-                                </div>
-                                <p className="record-amount-received">+$150.30</p>
-                                <p className="record-date">01/25/2025</p>
-                            </div>
-
-                        </div>
+                    <div className="add-bank">
+                        <GoPlus className="plus" />
+                        <p>Add Bank</p>
                     </div>
                 </div>
-                <div className="right-wrapper">
-                    <div className="profile-section">
-                        <div className="profile-cover">
-
-                        </div>
-                        <div className="profile-picture move-left">
-                            <input
-                                className="file-uploader"
-                                type="file"
-                                onChange={upload}
-                                accept="image/*"
-                            />
-                        </div>
-                        <div className="profile-bio move-left">
-                            <h2>Takunda Madziwa</h2>
-                            <p>tmadziwa@css.edu</p>
-                        </div>
+                <div className="transaction-section">
+                    <div className="transaction-header header">
+                        <h4>Transactions</h4>
+                        <button>View all</button>
                     </div>
-                    <div className="budget-section">
-                        <div className="budget-header header">
-                            <h4>Budgets</h4>
+                    <div className="transaction-wrapper">
+                        <div className="record">
+                            <div className="record-left">
+                                <img src={roger} alt="" />
+                                <p>Roger <span></span> </p>
+                            </div>
+                            <p className="record-amount-received">+$43.30</p>
+                            <p className="record-date">02/08/2025</p>
                         </div>
-                        <div className="budget-wrapper">
-                            <Doughnut
-                                data={{
-                                    labels: budgetData.map((data) => data.label),
-                                    datasets: [
-                                        {
-                                            label: "Budgets",
-                                            data: budgetData.map((data) => data.value),
-                                            backgroundColor: budgetData.map((data) => data.bColor),
-                                        },
-                                    ],
-                                }}
-                                options={{
-                                    plugins: {
-                                        legend: {
-                                            display: false
-                                        }
-                                    }
-                                }}
-                                className="pie-chart"
-                            />
+                        <div className="record">
+                            <div className="record-left">
+                                <img src={walmart} alt="" />
+                                <p>Walmart</p>
+                            </div>
+                            <p className="record-amount-spent">-$53.87</p>
+                            <p className="record-date">02/04/2025</p>
                         </div>
+                        <div className="record">
+                            <div className="record-left">
+                                <img src={netflix} alt="" />
+                                <p>Netflix</p>
+                            </div>
+                            <p className="record-amount-spent">-$13.99</p>
+                            <p className="record-date">02/02/2025</p>
+                        </div>
+                        <div className="record">
+                            <div className="record-left">
+                                <img src={chess} alt="" />
+                                <p>Chess.com</p>
+                            </div>
+                            <p className="record-amount-received">+$150.30</p>
+                            <p className="record-date">01/25/2025</p>
+                        </div>
+
                     </div>
                 </div>
             </div>
-      
+            <div className="right-wrapper">
+                <div className="profile-section">
+                    <div className="profile-cover">
+
+                    </div>
+                    <div className="profile-picture move-left">
+                        <input
+                            className="file-uploader"
+                            type="file"
+                            onChange={upload}
+                            accept="image/*"
+                        />
+                    </div>
+                    <div className="profile-bio move-left">
+                        <h2>Takunda Madziwa</h2>
+                        <p>tmadziwa@css.edu</p>
+                    </div>
+                </div>
+                <div className="budget-section">
+                    <div className="budget-header header">
+                        <h4>Budgets</h4>
+                    </div>
+                    <div className="budget-wrapper">
+                        <Doughnut
+                            data={{
+                                labels: budgetData.map((data) => data.label),
+                                datasets: [
+                                    {
+                                        label: "Budgets",
+                                        data: budgetData.map((data) => data.value),
+                                        backgroundColor: budgetData.map((data) => data.bColor),
+                                    },
+                                ],
+                            }}
+                            options={{
+                                plugins: {
+                                    legend: {
+                                        display: false
+                                    }
+                                }
+                            }}
+                            className="pie-chart"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
